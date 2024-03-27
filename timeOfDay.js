d0f21227a4e7c5aa2f3ae0fc950ef19d5950a84d
Erich Osterman <300088801@fvtc.edu>
2024-03-27 13:06:41 -0500

    First commit of Assignment 6

---
+++ timeOfDay.js
@@ -0,0 +1,27 @@
+
+const getTimeOfDay = (time = new Date().getHours()) => {
+    
+     if(time > 4 && time < 12) return "Morning"
+     else if (time >= 12 && time < 17) return "Afternoon"
+     else return "Evening"
+
+}
+
+const isMorning = (time) => {
+    if(time >= 4 && time < 12) return true
+    else return false
+}
+
+const isAfternoon = (time) => {
+    if(time >= 12  && time < 17) return true
+    else return false
+}
+
+const isEvening = (time) => {
+    if(time > 17 || time < 4) return true
+    else return false
+}
+
+
+
+module.exports = { getTimeOfDay , isMorning, isAfternoon, isEvening}
